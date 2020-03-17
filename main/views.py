89bb2ea65f6ae3bcaeb65874134c6c5d0a57811d
zpine <zpine99.email.com>
2020-03-17 19:21:18 +0800

    add come_soon.html

--- main/views.py
+++ main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def come_soon(request):
+    return render(request, 'main/come_soon.html')
